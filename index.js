6963af66011aab4b9c74f4a21d0beffc54cde154
Megan Rakow <meganrakow@Megans-MBP-2.lan>
2022-10-24 21:21:34 -0500

    adding names

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 //1.create a file = npm init
 //2. touch .gitignore
+import { generateSlug } from "random-word-slugs";
+import chalkAnimation from "chalk-animation";
+
+const length = parseInt(process.argv[2]);
+
+const bandName = generateSlug(length, { format: "title" });
+chalkAnimation.rainbow(bandName);
