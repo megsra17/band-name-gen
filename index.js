3120bef9bde02e795cd4f5de6c51fa9a4febce4f
Megan Rakow <meganrakow@Megans-MBP-2.lan>
2022-10-24 21:44:37 -0500

    adding names

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 //1.create a file = npm init
 //2. touch .gitignore
+import inquirer from "inquirer";
 import { generateSlug } from "random-word-slugs";
 import chalkAnimation from "chalk-animation";
 
-const length = parseInt(process.argv[2]);
+inquirer
+  .prompt([
+    {
+      type: "number",
+      name: "length",
+      message: "How long should the band name be",
+      default: 3,
+    },
+    {
+      type: "list",
+      name: "animationName",
+      message: "Which animation would you like",
+      default: "rainbow",
+      choices: ["rainbow", "pulse", "glitch", "radar", "neon", "karaoke"],
+    },
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    console.log(answers);
 
-const bandName = generateSlug(length, { format: "title" });
-chalkAnimation.rainbow(bandName);
+    const bandName = generateSlug(answers.length, { format: "title" });
+
+    if (answers.animationName === "pulse") {
+      chalkAnimation.pulse(bandName);
+    } else if (answers.animationName === "rainbow") {
+      chalkAnimation.rainbow(bandName);
+    } else if (answers.animationName === "neon") {
+      chalkAnimation.neon(bandName);
+    } else if (answers.animationName === "glitch") {
+      chalkAnimation.glitch(bandName);
+    } else if (answer.animationName === "radar") {
+      chalkAnimation.radar(bandName);
+    } else if (answers.animationName === "karaoke") {
+      chalkAnimation.karaoke(bandName);
+    }
+  })
+  .catch((error) => console.log(error));
